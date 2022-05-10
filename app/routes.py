c53aa6341acd91f096715b99bcce7ec66de2dcb3
AkmalFadlurohman <fadlurohmanakmal@rocketmail.com>
2022-05-10 14:02:51 +0800

    Create base front-end page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 # app/routes.py
 from app import app
+from flask import render_template, request, flash, redirect, url_for
 
 @app.route("/")
 @app.route("/index")
-def hello_world():
-	return "Hello World"
+def index():
+	return render_template("index.html")
