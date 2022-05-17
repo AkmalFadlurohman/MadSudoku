9b9dbf9a930ec92bdd4b3d5d806e58b1e6404958
AkmalFadlurohman <fadlurohmanakmal@rocketmail.com>
2022-05-18 01:51:55 +0800

    Migrate solution checker to server and implement challenge submission to server

--- app/result_handler.py
+++ app/result_handler.py
@@ -1,6 +1,7 @@
 from flask_restx import Resource
 from flask import request, abort
-import app.err_msg
+# import app.err_msg
+from app import err_msg
 from app.validator import Validator
 from app.models import Result, Challenge
 import json
@@ -23,11 +24,11 @@ class ResultHandler(Resource):
 			data = {}
 			for k in ('id', 'user_name', 'challenge_id', 'clear_time', 'clear_date'):
 				data[k] = dict[k]
-				
+
 			data['clear_date'] = data['clear_date'].strftime("%d/%m/%Y, %H:%M:%S")
 			data['clear_time'] = self.convert_from_mil_sec(data['clear_time'])
 			result_list.append(data)
-					
+
 #		for r in results:
 #			result_str += json.dumps({k:r.__dict__[k] \
 #					if k is not 'clear_date'\
@@ -36,21 +37,21 @@ class ResultHandler(Resource):
 #					+ ','
 #		result_str = '[' + result_str[:-1] + ']'
 		return result_list
-	
+
 	def post(self):
 		# 0. validation
 		json_body = request.data
 		flag, msg, code = self.validate_clear(json_body)
 		if not flag:
 			abort(code, msg)
-		
+
 		#1. convert json to dictionary and set values
 		body_dict = json.loads(json_body)
 		challenge_id = body_dict['challenge_id']
 		user_name = body_dict['user_name']
 		clear_time = self.convert_to_mil_sec(body_dict['clear_time'])
 		answer = body_dict['answer']
-		
+
 		# 2. check if the answer of user is correct
 		challenge = Challenge.query.filter_by(id=challenge_id).first()
 		if not challenge:
@@ -59,22 +60,22 @@ class ResultHandler(Resource):
 		is_clear = True
 		for y in range(9):
 			for x in range(9):
-				if solution[y][x] != answer[y][x]:
+				if solution[y][x] != int(answer[y][x]):
 					is_clear = False
 					break
 			if not is_clear:
 				break
-		
+
 		# 3.if it was incorrect, response false
 		if not is_clear:
 			return {"clear":False}
-		
+
 		# 4.if it was correct, save the result and reponse true
 		result = Result(user_name=user_name, challenge_id=challenge_id, clear_time=clear_time,
 				clear_date=datetime.now())
 		db.session.add(result)
 		db.session.commit()
-		
+
 		return {"clear":True}
 
 	def validate_clear(self, body):
@@ -82,20 +83,21 @@ class ResultHandler(Resource):
 		if not flag:
 			return False, msg, code
 		body_dict = json.loads(body)
-	
+
 		challenge_id = body_dict['challenge_id']
 		if challenge_id is None:
 			return False, err_msg.PARAM_EMPTY.format("challenge_id"), 400
-		if type(challenge_id) is not int:
-			return False, err_msg.MUST_INT.format("challenge_id"), 400
+		# if type(challenge_id) is not int:
+		# 	return False, err_msg.MUST_INT.format("challenge_id"), 400
+		challenge_id = int(challenge_id)
 		if challenge_id < 1:
 			return False, err_msg.MUST_POSITIVE_INT.format("challenge_id"), 400
-	
+
 		user_name = body_dict['user_name']
 		flag, msg, code = Validator.check_param(user_name, 'user_name')
 		if not flag:
 			return False, msg, code,
-	
+
 		clear_time = body_dict['clear_time']
 		if clear_time is None:
 			return False, err_msg.PARAM_EMPTY.format("clear_time"), 400
@@ -106,29 +108,28 @@ class ResultHandler(Resource):
 		#	return False, err_msg.MUST_INT.format("clear_time"), 400
 		#if clear_time < 1:
 		#	return False, err_msg.MUST_POSITIVE_INT.format("clear_time"), 400
-	
+
 		answer = body_dict['answer']
 		if not answer:
 			return False, err_msg.PARAM_EMPTY.format("answer"), 400
 		if type(answer) is not list:
 			return False, err_msg.MUST_LIST.format("asnwer"), 400
-	
+
 		return True, None, None
 
 	def convert_to_mil_sec(self, time_str):
 		hour = 60 * 60 * 1000
 		minute = 60 * 1000
 		second = 1000
-		time = time_str.split(":")	
+		time = time_str.split(":")
 		return int(time[0]) * hour + int(time[1]) * minute + int(time[2]) * second
 
 	def convert_from_mil_sec(self, mil_sec):
 		hour = 60 * 60 * 1000
 		minute = 60 * 1000
 		second = 1000
-		
+
 		h = str(int(mil_sec/hour)).zfill(2)
 		m = str(int((mil_sec%hour)/minute)).zfill(2)
 		s = str(int((mil_sec%minute)/second)).zfill(2)
 		return h+":"+m+":"+s
-
